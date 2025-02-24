cecb887b87de90a8a5e0491c6f56a772d8465f69
Paulo-SenacRS <prjunior@senacrs.com.br>
2025-02-24 12:57:46 -0300

    ajustes conteudo 6

--- 06/atividades/relacionar_colunas/relacionar-colunas.js
+++ 06/atividades/relacionar_colunas/relacionar-colunas.js
@@ -21,23 +21,58 @@ $(function (){
                 $(".selecionado").removeClass("selecionado");
                 $("#associar_colulas_errou").modal("show")
                 erro.play()
-               
             }
         }
-        
+    });
+
+    $(".resp02, .item02").click(function (){
+        $(this).toggleClass("selecionado");
+
+        if($(".selecionado").length == 2){
+            console.log($(".selecionado")[0].dataset.resp02, $(".selecionado")[1])
+
+            if($(".selecionado")[0].dataset.resp02 == $(".selecionado")[1].dataset.resp02)
+            {
+                $(".selecionado").prop("disabled", "disabled")
+                $(".selecionado").addClass("acertou")
+                $(".selecionado .txtacao").text($(".selecionado")[0].dataset.resp02)
+                $(".selecionado").removeClass("selecionado");
+                $("#associar_colulas_acertou").modal("show")
+                acerto.play()
+            }else
+            {
+                // $(".selecionado").addClass("errou")
+                $(".selecionado").removeClass("selecionado");
+                $("#associar_colulas_errou").modal("show")
+                erro.play()
+            }
+        }
     });
 
     randomizeResp(50);
+    randomizeResp02(50);
 })
 
-function randomizeResp(total){
 
+function randomizeResp(total){
     let resp = Array.from(document.querySelectorAll('.resp'))
 
     for (let i = 0; i < total; i++){
         $(resp).each(
             function(){ 
                 $(this).insertBefore($(resp[Math.floor(Math.random()*resp.length - 1)]))
+            }
+        )
+    }
+}
+
+function randomizeResp02(total){
+    let resp02 = Array.from(document.querySelectorAll('.resp02'))
+
+    for (let i = 0; i < total; i++){
+        $(resp02).each(
+            function(){ 
+                $(this).insertBefore($(resp02[Math.floor(Math.random()*resp02.length - 1)]))
             }
         )
     }
